3dff9beff377f2106c18d348e0564f167127d9b0
Tyler Hecht <50639776+RadiateGold@users.noreply.github.com>
2021-12-28 18:26:53 -0500

    Fixed bugs with castling and h-file captures

--- MusicalChess.py
+++ MusicalChess.py
@@ -1,6 +1,18 @@
 from midiutil import MIDIFile
 from pgn_parser import parser, pgn
 
+# example PGN
+PGN = """
+1. e4 e5 2. Nf3 Nc6 3. Bb5 a6 {This opening is called the Ruy Lopez.}
+4. Ba4 Nf6 5. O-O Be7 6. Re1 b5 7. Bb3 d6 8. c3 e4 9. h3 Nb8 10. d4 Nbd7
+11. c4 c6 12. cxb5 axb5 13. Nc3 Bb7 14. Bg5 b4 15. Nb1 h6 16. Bh4 c5 17. dxe5
+Nxe4 18. Bxe7 Qxe7 19. exd6 Qf6 20. Nbd2 Nxd6 21. Nc4 Nxc4 22. Bxc4 Nb6
+23. Ne5 Rae8 24. Bxf7+ Rxf7 25. Nxf7 Rxe1+ 26. Qxe1 Kxf7 27. Qe3 Qg5 28. Qxg5
+hxg5 29. b3 Ke6 30. a3 Kd6 31. axb4 cxb4 32. Ra5 Nd5 33. f3 Bc8 34. Kf2 Bf5
+35. Ra7 g6 36. Ra6+ Kc5 37. Ke1 Nf4 38. g3 Nxh3 39. Kd2 Kb5 40. Rd6 Kc5 41. Ra6
+Nf2 42. g4 Bd3 43. Re6 1/2-1/2
+"""
+
 # set up midi
 MyMIDI = MIDIFile(1)
 MyMIDI.addTempo(0, 0, 60)
@@ -10,10 +22,10 @@ def pgn_to_midi(PGN: str):
     game = parser.parse(PGN, actions=pgn.Actions())
     time = 0
     for i in range(1, get_total_moves(game)+1):
-        ply_to_note(get_ply(str(game.move(i)), "white"), time)
+        ply_to_note(str(game.move(i).white.san), time)
         time += 1
         try:
-            ply_to_note(get_ply(str(game.move(i)), "black"), time)
+            ply_to_note(str(game.move(i).black.san), time)
             time += 1
         except:
             continue
@@ -33,27 +45,17 @@ def get_total_moves(game: str) -> int:
         finally:
             move = move + 1
 
-def get_ply(move: str, color: str) -> str:
-    spaces = []
-    index = 0
-    # find where the spaces are
-    for char in move:
-        if char == " ":
-            spaces.append(index)
-        index += 1
-    # return the appropriate ply
-    if color == "white":
-        return move[spaces[0]+1:spaces[1]]
-    elif color == "black":
-        return move[spaces[1]+1:]
-
 def ply_to_note(ply: str, time: int):
-    instrument = piece_to_instrument(get_piece(ply))
-    volume = rank_to_volume(get_rank(ply))
-    pitch = add_accidental(file_to_pitch(get_file(ply)), ply)
-    # add notes
-    MyMIDI.addProgramChange(0, 0, time, instrument)
-    MyMIDI.addNote(0, 0, pitch, time, 1, volume)
+    if not "O" in ply:
+        instrument = piece_to_instrument(get_piece(ply))
+        volume = rank_to_volume(get_rank(ply))
+        pitch = add_accidental(file_to_pitch(get_file(ply)), ply)
+        # add notes
+        MyMIDI.addProgramChange(0, 0, time, instrument)
+        MyMIDI.addNote(0, 0, pitch, time, 1, volume)
+    else:
+        MyMIDI.addProgramChange(0, 0, time, 1)
+        MyMIDI.addNote(0, 0, 0, time, 1, 0)
     
 def get_piece(ply: str) -> str:
     if ply[0] in ["N", "B", "R", "Q", "K"]:
@@ -125,10 +127,11 @@ def file_to_pitch(file: str) -> int:
         return 0
     
 def add_accidental(pitch: int, ply: str) -> int:
-    if "x" in ply:
-        pitch -= 1
-    if "=" in ply:
-        pitch -= 2
+    if pitch > 0:
+        if "x" in ply:
+            pitch -= 1
+        if "=" in ply:
+            pitch -= 2
     if "+" in ply:
         pitch += 1
     if "#" in ply:
@@ -136,4 +139,4 @@ def add_accidental(pitch: int, ply: str) -> int:
     return pitch
 
 # testing
-pgn_to_midi("1. e4 d5 2. Nf3 Bc6 3. Bb5 a6")
+pgn_to_midi(PGN)
